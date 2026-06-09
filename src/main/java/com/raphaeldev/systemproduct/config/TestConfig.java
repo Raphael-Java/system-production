1eb157e9b27bd36123173f794e2734a1626bb7f1
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-06-09 13:01:14 -0300

    Creating the payment class with one-to-one association

--- src/main/java/com/raphaeldev/systemproduct/config/TestConfig.java
+++ src/main/java/com/raphaeldev/systemproduct/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.raphaeldev.systemproduct.entities.Category;
 import com.raphaeldev.systemproduct.entities.Order;
 import com.raphaeldev.systemproduct.entities.OrderItem;
+import com.raphaeldev.systemproduct.entities.Payment;
 import com.raphaeldev.systemproduct.entities.Product;
 import com.raphaeldev.systemproduct.entities.User;
 import com.raphaeldev.systemproduct.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}	
 }
